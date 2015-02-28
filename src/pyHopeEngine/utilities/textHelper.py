95961d80e879aeefffdda1a74dcead7977b45633
Trinak <devon.arrington@gmail.com>
2015-02-27 19:27:06 -0800

    Changing text helper

--- src/pyHopeEngine/utilities/textHelper.py
+++ src/pyHopeEngine/utilities/textHelper.py
@@ -5,6 +5,9 @@ Created on Jul 1, 2013
 
 Creates text that can be wrapped, justified, 
 colored, and other options using the Glyph module.
+
+Example:
+{defaultFont; {green; Put Text Here}}
 '''
 
 from glyph import Macros, Glyph
@@ -21,10 +24,12 @@ Macros['teal'] = ('color', ECOM.Colors.TEAL)
 Macros['olive'] = ('color', ECOM.Colors.OLIVE)
 Macros['purple'] = ('color', ECOM.Colors.PURPLE)
 Macros['silver'] = ('color', ECOM.Colors.SILVER)
+Macros['smallerFont'] = ('font', SysFont('tahoma', 12))
+Macros['smallFont'] = ('font', SysFont('tahoma', 16))
 Macros['defaultFont'] = ('font', SysFont('tahoma', 24))
 Macros['largeFont'] = ('font', SysFont('tahoma', 32))
 Macros['largerFont'] = ('font', SysFont('tahoma', 48))
-        
+
 def createText(text, rect, justify = None):
     glyph = Glyph(rect)
     glyph.input(text, justify)
